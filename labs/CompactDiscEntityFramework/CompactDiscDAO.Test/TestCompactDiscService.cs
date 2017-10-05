bdc3465287d5aba1ca3591eded797735701f0c8b
nick.todd@conygre.com <nick.todd@conygre.com>
2017-10-05 14:48:34 +0100

    Added a test project

---
+++ labs/CompactDiscEntityFramework/CompactDiscDAO.Test/TestCompactDiscService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace CompactDiscDAO.Test
+{
+    [TestClass]
+    public class TestCompactDiscService
+    {
+        [TestMethod]
+        public void AddCompacDiscTriggersAddMethodCallOnDao()
+        {
+            // arrange
+            var mockDao = new Mock<CompactDiscDao>();
+            CompactDiscService serviceUnderTest = new CompactDiscService();
+            serviceUnderTest.DiscDao = mockDao.Object;
+
+            // act
+            serviceUnderTest.GetCatalog();
+
+            // assert
+            mockDao.Verify(mock => mock.GetAllDiscs(), Times.Once());
+
+        }
+    }
+}
